6f3dc3a02f281fc7d7f3b52d640890f013565a33
Akshay <53929892+badcoderxd@users.noreply.github.com>
2020-11-28 13:12:53 +0530

    Add files via upload

---
+++ public/sidebar.js
@@ -0,0 +1,12 @@
+function openNav()
+{
+    
+    document.getElementsByClassName("sidenav").style.visibility = "visible";
+
+}
+   function closeNav()
+   {
+
+     document.getElementsByClassName("sidenav").style.visibility = "hidden";
+
+   }
